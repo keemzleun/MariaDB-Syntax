1f91c9aea5e2e515015ff627b3873c60710c14f9
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-05-16 16:46:15 +0900

    1.ddl

--- 1.DDL.sql
+++ 1.DDL.sql
@@ -27,3 +27,25 @@ SHOW CREATE TABLE author;
 
 -- post 테이블 신규 생성
 CREATE TABLE posts (id INT PRIMARY KEY, title VARCHAR(255), content VARCHAR(255), author_id INT, Foreign Key (author_id) REFERENCES author(id));
+
+-- 테이블 인덱스 조회
+SHOW INDEX FROM author;
+
+-- ALTER문 : 테이블의 구조를 변경
+-- 테이블 이름 변경
+ALTER TABLE posts RENAME post;
+
+-- 테이블 컬럼 추가
+ALTER TABLE author ADD COLUMN test1 VARCHAR(50);
+
+-- 테이블 컬럼 삭제
+ALTER TABLE author DROP COLUMN test1;
+
+-- 테이블 컬럼명 변경
+ALTER TABLE post CHANGE content contents VARCHAR(255);
+
+-- 테이블 컬럼 타입과 제약조건 변경
+ALTER TABLE author MODIFY COLUMN email VARCHAR(255) NOT NULL;
+
+-- 테이블 삭제
+DROP TABLE 테이블명;
